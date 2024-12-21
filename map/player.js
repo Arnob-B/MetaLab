bbc1b549e6758b857b1c0bafcdb9e8c25a871166
tester <test@gmail.com>
2024-12-21 22:22:10 +0530

    server change

--- map/player.js
+++ map/player.js
@@ -1,43 +1,43 @@
 import map from "./map.js"
-export default class Player{
-    constructor(canvasHeight, canvasWidth){
-        // 32 x 32 player
-        this.width = 32;
-        this.height = 32;
+export default class Player {
+  constructor(canvasHeight, canvasWidth) {
+    // 32 x 32 player
+    this.width = 32;
+    this.height = 32;
 
-        // placing player in the middle of the screen
-        // this.x = canvasWidth/2 - this.width/2;
-        this.x=2;
-        this.y=1;
-        // this.y = canvasHeight/2 - this.height/2;
+    // placing player in the middle of the screen
+    // this.x = canvasWidth/2 - this.width/2;
+    this.x = 2;
+    this.y = 1;
+    // this.y = canvasHeight/2 - this.height/2;
 
-        // for spritesheet animations
-        this.frameX = 1;
-        this.frameY = 5;
+    // for spritesheet animations
+    this.frameX = 1;
+    this.frameY = 5;
 
-        // player sprite
-        this.img = document.getElementById("player");
+    // player sprite
+    this.img = document.getElementById("player");
+  }
+
+  update(x, y) {
+    this.frameY = (this.frameY + 1) % 5;
+    this.frameX = (this.frameX + 1) % 5;
+    this.x += x;
+    this.y += y;
+    document.querySelector("#playerLog").innerHTML = ` x->${this.x},y->${this.y}`
+  }
+
+  draw(ctx, camera) {
+    //console.log("draw");
+
+    // drawImage(sourceimg, sourcx, source y, source width , source heigh, canvx, canvy, dispwidth, dispwidth)
+    if (((this.x * this.width >= camera.x) && (this.x * this.width <= camera.x + camera.canvasWidth))
+      &&
+      ((this.y * this.height >= camera.y) && (this.y * this.height <= camera.y + camera.canvasHeight))
+    ) {
+      //console.log("here");
+      ctx.drawImage(this.img, this.frameX * 64, this.frameY * 64, 64, 64, this.x * this.width - camera.x, this.y * this.height - camera.y, 64, 64);
     }
-
-    update(x, y){
-      this.frameY = (this.frameY + 1)%5;
-      this.frameX = (this.frameX + 1)%5;
-      this.x += x;
-      this.y += y;
-        document.querySelector("#playerLog").innerHTML = ` x->${this.x},y->${this.y}`
-    }
-
-    draw(ctx,camera){
-        //console.log("draw");
-
-      // drawImage(sourceimg, sourcx, source y, source width , source heigh, canvx, canvy, dispwidth, dispwidth)
-      if(((this.x*this.width >= camera.x )&& (this.x*this.width <= camera.x + camera.canvasWidth))
-        &&
-        ((this.y*this.height >=camera.y )&& (this.y*this.height <= camera.y + camera.canvasHeight))
-      )
-      {
-        //console.log("here");
-        ctx.drawImage(this.img, this.frameX * 64, this.frameY * 64, 64, 64, this.x*this.width - camera.x, this.y*this.height - camera.y, 32, 32);}
-    }
+  }
 
 }
--- map/staticServer/server.js
+++ map/staticServer/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const server = express();
-server.use(express.static("./../"));
-server.listen(3000,()=>{
+server.use(express.static("./"));
+server.listen(3000, () => {
   console.log("serving on 3000");
 });
